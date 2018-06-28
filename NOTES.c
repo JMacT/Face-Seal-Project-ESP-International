961ec22d7e722a77a1173edb0240bf9bbde8676e
JMacT <jessemacfarlane@gmail.com>
2018-06-28 14:50:43 -0500

    Second Upload
    Attempted to parse the variable string::line into another line using the function in the wiki. It was a failure. Trying to pass the info to an external function that will do the same task now.

---
+++ NOTES.c
@@ -0,0 +1,12 @@
+/*
+                dash_Holder = line.substr(0,4);
+
+                CS_Inch = line.substr(x,x);
+                CST_Inch = line.substr(x,x);
+                ID_Inch = line.substr(x,x);
+                IDT_Inch = line.substr(x,x);
+
+                CS_mm = line.substr(x,x);
+                CST_mm = line.substr(x,x);
+                ID_mm = line.substr(x,x);
+                IDT_mm = line.substr(x,x);
--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
+
+void Variable_Assigner(string source, char matrix[][]);
+
+
+
 
 int main()
 {
     string search;
     string line;
+    char var_Matrix[9][5];
+
+
     ifstream referenceOne;
     referenceOne.open ("as568.txt");
     size_t pos;
@@ -17,15 +26,32 @@ int main()
     search = "-"+search;
 
     if (referenceOne.is_open() ) {
-        while( getline(referenceOne, line) ){ //pulls in one line from as568.txt
-            pos = line.find(search); //search for
+        while( getline(referenceOne, line) ){ //line.string = (entire) as568.txt
+            pos = line.find(search); //line.string now = the correct dash size entry
             if(pos!=string::npos){
-                cout << line;
-            referenceOne.close();
+
+                Variable_Assigner(line, var_Matrix); //sort information into the matrix
+
             }
         }
+
         referenceOne.close();
     }
     else cout << "unable to open file.\n";
     return 0;
 }
+
+void Variable_Assigner(string source, char matrix[][]){ //matrix is set up FOR 9 DIMENSIONS TYPES with 5 characters for DATA.DATA [9][5]
+    int i,j = 0;
+    int k = -1;
+    for(i=0;i<70;i++){
+        k++;
+
+        if(source[i]=='\t'){
+            k++;
+        }
+        else{
+            matrix[j][k] = source [i];
+        }
+    }
+};
