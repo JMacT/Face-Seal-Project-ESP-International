04f9f749ef7f84c03b48f098c1bf5b06fb1a6364
JMacT <jessemacfarlane@gmail.com>
2018-07-02 11:28:32 -0500

    Finally got STOF() working
    Now able to save dimenions. Huge success time to start naming variables and processing them.

--- main.cpp
+++ main.cpp
@@ -9,33 +9,6 @@
 
 using namespace std;
 
-void Variable_Assigner(string source);
-
-/*
-float stof(const char* s){
-  float rez = 0, fact = 1;
-  if (*s == '-'){
-    s++;
-    fact = -1;
-  };
-  for (int point_seen = 0; *s; s++){
-    if (*s == '.'){
-      point_seen = 1;
-      continue;
-    };
-    int d = *s - '0';
-    if (d >= 0 && d <= 9){
-      if (point_seen) fact /= 10.0f;
-      rez = rez * 10.0f + (float)d;
-    };
-  };
-  return rez * fact;
-};
-*/
-
-
-float str2float(char){
-};
 
 
 int main()
@@ -48,6 +21,8 @@ int main()
     char var;
     int i, j=0;
 
+    float holder=0; // this is where the float goes
+    char temp2[6]; //this is the array that passes to str2float
 
     ifstream referenceOne;
     referenceOne.open ("as568.txt");
@@ -92,19 +67,20 @@ int main()
                     }
                     */
 
-                for(i=0;i<9;i++){
+                for(i=0;i<9;i++){ //0-8 slots for all 8 possible dimensions of the array
                     //temp = storage[i];
-                    float holder=0; // this is where the float goes
-                    char temp2[10]; //this is the array that passses to str2float
-                    for (j=0;j<7;j++){
-                        temp2[j]=storage[i][j];
+                    for (j=0;j<6;j++){
+                        if((i==0 && j==4) ||(i==0 && j==5) ){
+                            temp2[4] = ' '; // doesn't put nonsense after the dash number
+                            temp2[5] = ' ';
+                        }
+                        else {
+                            temp2[j]=storage[i][j]; //Note: forced space or '0' for unused slots
+                        }
                     }
-                    int k=0;
-                    holder = str2float (temp2[i+1]); //Not Tested, need WINGC 2011 for ATOF.
-                    for(k=0;k<5;k++){
-                        cout << temp2[k];
-                    }
-                    cout << holder << "this is holder\n";
+                    holder = atof(temp2);
+                    cout << "this is dimensions times 4 : " << (holder*4)  << endl;
+
 
                 }
 
@@ -116,50 +92,3 @@ int main()
     else cout << "unable to open file.\n";
     return 0;
 }
-
-
-float str2float(char* s){
-// solve for special cases where s begins with 0's or nun numeric values, or if s is NULL
-  float result = 0;
-  int decimalCount = 0, i = 0, decimalPointLoc = strlen(s);
-  for (; s[i] != '\0'; i++){
-    if (s[i] == '.') decimalPointLoc = i;
-    if (i < decimalPointLoc) { result *= 10; result += (int)(s[i] + '0'); }
-    else { result += (float)(s[i] + '0')/(pow(i-decimalPointLoc,10)); }
-  }
-  return result;
-}
-
-
-/*
-void Variable_Assigner(string source){ //matrix is set up FOR 9 DIMENSIONS TYPES with 5 characters for DATA.DATA [9][5]
-    int i,j = 0;
-    string storage[9]={0};
-    string temp;
-
-    cout << source;
-/*
-    for(i=0;i<50;i++){
-        if (source[i]=='\t'){
-            storage[j]=temp;
-            j++;
-
-            if(j==8){
-                i=50; //exit if all the dimensions are full
-            }
-
-        }
-
-        else{
-            //temp = temp + source[i];
-            cout << temp <<endl;
-        }
-
-    }*/
-
-   /* for(i=0;storage[i][0]!='\0'; i++){
-        for(j=0;storage[i][j]!='\0';j++){
-            cout << storage[i][j];
-        }
-    }*/
-//};
