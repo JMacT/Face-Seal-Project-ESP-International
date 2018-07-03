98416b2130eecd1ff66645506327b0b2c5552f1a
JMacT <jessemacfarlane@gmail.com>
2018-07-03 13:10:17 -0500

    Moving on to cross-section test
    All basic functions intact. Moving on to cross section test

--- main.cpp
+++ main.cpp
@@ -7,28 +7,84 @@
 using namespace std;
 
 void hardware_specs(float input_arr [] ); //cin customer hardeware specs. Output an array of those specs
-float dash_Query(string as568); //search as568.txt for an oring size and output an array of those specs
+float * dash_Query(string as568); //search as568.txt for an oring size and output an array of those specs
+void CS_Test(float*, float*);
+void string_to_float_array(string, float*, int, int);
 
 
 int main()
 {
     string search; //Holds dash size required for the search
-    float hardware[9]={0}; //Customer input: Units, P_direction, Media, ID, ID_tol, OD, OD_tol, Gland_Max, Gland_tol, '\0'
+    float *as568; //a pointer to the array holding all info on a dash size. See below:
+    //AS568 Format - used for hard coding all other functions using the as568 resource.
+    //as568[0] : Dash Size Callout
+    //as568[1] : CS inch
+    //as568[2] : CS Tol inch
+    //as568[3] : ID inch
+    //as568[4] : ID Tol inch
+    //as568[5] : CS mm
+    //as568[6] : CS Tol mm
+    //as568[7] : ID mm
+    //as568[8] : ID Tol mm
 
+    float hardware[9]={0}; //Customer input: Units, P_direction, Media, ID, ID_tol, OD, OD_tol, Gland_Max, Gland_tol, '\0'
+    float CS_array[]={0.040, 0.050, 0.060, 0.070, 0.103, 0.139, 0.210, 0.275};//lists all AS568 CSs available for fitment. Because of -0xx and -9xx c/s variance, hard coding may be better option
+                //NOTE: Cannot judge the following CS sizes (2-9xxs) because they are not covered by Parker recommendations: 0.056, 0.064, 0.072, 0.078, 0.082, 0.087, 0.097, 0.116, 0.118
 
     hardware_specs(hardware);
+    cout << hardware[2] << endl;//OD tol
 
-    cout << "What dash size do you THINK will work?\n"; //choose a dash size
+    //CS_Test(hardware, CS_array);
+
+    cout << "Which dash size do you want analyzed\n"; //choose a dash size
     cin >> search;
+
     search = "-" + search; //Adds the dash in order to search as568.txt
 
-    cout << "The dash size to be analyzed is : " << dash_Query(search) << endl; //uses dash size to search as568.txt and get an array of floats containing dim/tols for that size
+    as568 = dash_Query(search); //*as568 points to a returned array of all vals associated with as568.txt under the search term
+
+    cout << "The dash size to be analyzed is : " << as568[0] << endl; //uses dash size to search as568.txt and get an array of floats containing dim/tols for that size
 
     return 0;
 }
 
+/*
+string_to_float_array
+Inputs: a string, a float array, an integer width, a loop escape value
+Function: Fill each index of the float array with one snip of the string separating by \t
+*/
+void string_to_float_array(string line, float arr[], int w, int escape){
+    //string line;
+    string temp;
+    int i, j=0;
+
+    for(i=0;i<w;i++){
+        //data is separated by '\t'.
+        //This splits data and then copies it as a float into arr[].
+        while( (line[(j+i)]!= '\t' ) && (line[(j+i)]!= '\0' ) ){
+            temp = temp + line[(j+i)];
+            j++;
+        }
+        arr[i] = stof(temp);
+
+        temp.clear();
+
+        //If you have filled all of the dimensions...
+        if( (j+i) == line.length()){
+            i=w;
+        }
+    }
+    //END
+
+    //cout << "This is a test of sting_to_float function. This is arr[5] : " << arr[5] << endl;
+
+    return;
+};
+
 //cin customer hardeware specs. Output an array of those specs
 /*
+Input:
+-1. an empty array from main()
 0. Metric (0) or Imperial Measurements (1)
 1. Pressure Direction: Internal pressure (0) or External Pressure (1)
 2. Media: Liquid (0) or Gas (1)
@@ -38,6 +94,7 @@ int main()
 6. OD Tolerance
 7. Maximum gland depth allowable
 8. Gland cut tolerance
+Working? YES 7/3/18
 */
 void hardware_specs(float input_arr []){
     //defaults to metric dimensions
@@ -87,15 +144,45 @@ void hardware_specs(float input_arr []){
     return;
 };
 
+/*
+CS_Test
+Inputs: the customer's hardware in array format, the array of available cross-sections (hard-coded from as568 reference material)
+Function: Test the available cross-sections in the customer's hardware and see which are viable options
+Results: The array of available cross sections is left without those that will not work
+*/
+void CS_Test(float hardware[], float CS_array[]){
+    float G_actual_max, L_actual;
+    string line;
+    string temp;
+    //string search;
 
-float dash_Query(string as568){
 
-    string storage[9]={}; //Holds only pertinent characters read from string "line"
+    ifstream refTwo;
+    refTwo.open("Parker Oring Face Seal Design Chart 4-3.txt"); // opens the face seal design recommendations .txt file
+
+
+
+    refTwo.close(); //closes the file
+
+    return;
+};
+
+/*
+dash_Query:
+Input: an as568 dash size
+Function: to pull size's line from as568.txt and copy the values to an array in float form.
+Returns: The address of an array containing that dash size's data in float form.
+Working? YES 7/3/18
+*/
+float * dash_Query(string as568){
+
     string temp; //Used to fill storage and exit loop in case of \t in string "line"
     string line; //Holds info from as568.txt
-    float dimTol[8] = {0}; //stof directly from the string "storage" indexed by i
+    static float dimTol[9] = {0}; //stof directly from the string "storage" indexed by i
     size_t pos;
     int i, j=0; //counters
+    int width = 9; //Exit the loop when all 9 dimensions are filled
+    int exit_var = 8; //Exit the loop when data is 8 bits long
 
     ifstream refOne;
     refOne.open ("as568.txt"); //Opening as568.txt as reference one
@@ -104,48 +191,20 @@ float dash_Query(string as568){
     while( getline(refOne, line) ){ //line.string = (entire) as568.txt
         pos = line.find(as568); //line.string now = the correct dash size entry
 
+        //string_to_float_array(line, dimTol, width, exit_var);
         if(pos!=string::npos){
 
             cout << line << endl; //Debug -- look at data is in "line" string.
 
+            string_to_float_array(line, dimTol, width, exit_var); //fill dimTol with floats of all the dash data
 
-                //Fill 1D string array "storage" with string data from the dash size line, breaking at '\t' character
-                for(i=0;i<9;i++){
-
-                    if (line[i]=='\t'){
-
-                        dimTol[j] = stof(temp);
-                        //cout << "this is dimTol at index " << j << " : " << dimTol[j] << endl;
-                        temp.clear();
-                        j++;
-
-                        if(j==8){
-                            i=500; //exit if all the dimensions are full
-                        }
-
-                    }
-
-                    else{
-                        temp = temp + line[i];
-                    }
-
-                }
-                //END
-
-    /*
-                //Clean up data in 1D string array "storage"
-                //Also, copy that data as a float into dimTol locations 0-8 (mirrors format of storage string)
-                for(i=0;i<9;i++){ //0-8 slots for all 8 possible dimensions of the array
-                    dimTol[i]= stof(storage[i]);
-                }
-                //END
-    */
+            break; //Exit
 
         }
     }
 
     refOne.close(); //close as568.txt
 
-    return dimTol[0];
+    return dimTol;
 };
 
