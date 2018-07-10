03f4d6c7c854bdd07acfc19975e08124b70f4629
JMacT <jessemacfarlane@gmail.com>
2018-07-10 15:00:09 -0500

    Initial commit
    Still time to turn back.

---
+++ includes/DashMath.h
@@ -0,0 +1,16 @@
+#ifndef DASHMATH_H
+#define DASHMATH_H
+
+
+class DashMath
+{
+    public:
+        DashMath(float, float, float, float);
+        float getVolume();
+        float id, id_tol, cs, cs_tol, volume;
+    protected:
+
+    private:
+};
+
+#endif // DASHMATH_H
---
+++ includes/HardwareMath.h
@@ -0,0 +1,25 @@
+#ifndef HARDWAREMATH_H
+#define HARDWAREMATH_H
+
+
+class HardwareMath
+{
+    public:
+        getMaxVolume(float); //Pass the gland max from Parker reqs txt into this
+        getMinVolume(float); //Pass the gland min from Parker reqs txt into this
+        int units;
+        int p_direction;
+        int media;
+        float id;
+        float id_tol;
+        float od;
+        float od_tol;
+        float gland_max;
+        float gland_tol;
+
+    protected:
+
+    private:
+};
+
+#endif // HARDWAREMATH_H
